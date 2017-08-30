d626f52eb6c31f8f9243b627f9d1e967ff89c735
Nicholas Totsch <nicholas.totsch@gmail.com>
2017-08-29 23:06:17 -0700

    Delete exceptions.py

--- ethapi/exceptions.py
+++
@@ -1,6 +0,0 @@
-class UnimplementedException(Exception):
-    """
-    When running a method that hasn't been implemented upstream or downstream
-    """
-    def __str__(self):
-        return "The requested operation is not implemented."
